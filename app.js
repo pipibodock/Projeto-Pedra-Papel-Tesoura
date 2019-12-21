fda2b9f39dac629efa57542dccb7950211677246
Luiz Fellipe <fellipe.mirandamatos@gmail.com>
2019-12-21 09:45:18 -0300

    Finaliza JavaScript
    - Implementa as funções win, draw e lose

--- app.js
+++ app.js
@@ -1,9 +1,79 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".pontuacao");
-const result_div = document.query_Selector(".result");
+const scoreBoard_div = document.querySelector(".score");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
+const smallUserWord = "user".fontsize(3).sup();
+const smallCompWord = "comp".fontsize(3).sup();
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+  if (letter == "r") return "Rock";
+  if (letter == "p") return "Paper";
+  return "Scissors";
+}
+
+function colorFeedback(userChoice, color_class) {
+  userChoice_id = document.getElementById(userChoice);
+  userChoice_id.classList.add(color_class);
+  setTimeout(() => userChoice_id.classList.remove(color_class), 500)
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You Win!`;
+  colorFeedback(userChoice, 'green-glow')
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You Lost!`;
+  colorFeedback(userChoice, 'red-glow')
+}
+
+function draw(userChoice, computerChoice) {
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}. Draw!`;
+  colorFeedback(userChoice, 'gray-glow')
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "pr":
+    case "sp":
+    case "rs":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "pp":
+    case "ss":
+    case "rr":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+function main() {
+  rock_div.addEventListener('click', () => game("r"));
+  paper_div.addEventListener('click', () => game("p"));
+  scissor_div.addEventListener('click', () => game("s"))
+}
+
+main();
